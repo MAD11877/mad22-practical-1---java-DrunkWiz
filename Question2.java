c3ab289a94b9e87c29d3e3e2d46a0feebd2e95c2
DrunkWiz <32932464+DrunkWiz@users.noreply.github.com>
2022-04-20 01:35:19 +0800

    Update Question2.java
    Remove whitespace

--- Question2.java
+++ Question2.java
@@ -9,7 +9,6 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
     Scanner in = new Scanner(System.in);
     System.out.println( "Enter your height");
     double height = in.nextDouble();
